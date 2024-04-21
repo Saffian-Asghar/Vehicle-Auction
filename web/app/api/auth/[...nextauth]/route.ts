a2049ce48e21908285549b24668dc6d720ead7e7
Saffian Asghar <baigsaffian@gmail.com>
2024-04-21 02:27:22 +0200

    test api call OK

--- web/app/api/auth/[...nextauth]/route.ts
+++ web/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
-import DuendeIdentityServer6 from "next-auth/providers/duende-identity-server6";
+import NextAuth, { NextAuthOptions } from "next-auth"
+import DuendeIdentityServer6 from 'next-auth/providers/duende-identity-server6';
 
-export const authOptions : NextAuthOptions  = {
+export const authOptions: NextAuthOptions = {
     session: {
         strategy: 'jwt'
     },
@@ -15,7 +15,24 @@ export const authOptions : NextAuthOptions  = {
             idToken: true
         })
     ],
-};
+    callbacks: {
+        async jwt({token, profile, account}) {
+            if (profile) {
+                token.username = profile.username
+            }
+            if (account) {
+                token.access_token = account.access_token
+            }
+            return token;
+        },
+        async session({session, token}) {
+            if (token) {
+                session.user.username = token.username
+            }
+            return session;
+        }
+    }
+}
 
-const  handler  = NextAuth(authOptions);
+const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST }
--- web/types/next-auth.d.ts
+++ web/types/next-auth.d.ts
@@ -1,19 +1,20 @@
-import { DefaultSession } from "next-auth";
+import {DefaultSession} from 'next-auth';
 
-declare module "next-auth" {
-    interface Session  {
+declare module 'next-auth' {
+    interface Session {
         user: {
-            username: string;
-        } & DefaultSession["user"];
+            username: string
+        } & DefaultSession['user']
     }
 
     interface Profile {
-        username: string;
+        username: string
     }
 }
 
-declare module "next-auth/jwt" {
+declare module 'next-auth/jwt' {
     interface JWT {
-        username: string;
+        username: string
+        access_token?: string
     }
 }
