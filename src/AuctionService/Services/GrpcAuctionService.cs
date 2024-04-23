a34c5a3b4ad9cd06455ee220feca05b7cf6aba33
Saffian Asghar <baigsaffian@gmail.com>
2024-04-23 20:47:46 +0200

    Adding gRPC to AuctionService (server)

---
+++ src/AuctionService/Services/GrpcAuctionService.cs
@@ -0,0 +1,36 @@
+﻿using AuctionService.Data;
+using Grpc.Core;
+
+namespace AuctionService;
+
+public class GrpcAuctionService : GrpcAuction.GrpcAuctionBase
+{
+    private readonly AuctionDBContext _dBContext;
+
+    public GrpcAuctionService(AuctionDBContext dBContext)
+    {
+        _dBContext = dBContext;
+    }
+
+    public override async Task<GrpcAuctionResponse> GetAuction(GetAuctionRequest request, ServerCallContext context)
+    {
+        Console.WriteLine($"==> GRPC ==> GetAuction: {request.Id}");
+
+        var auction = await _dBContext.Auctions.FindAsync(Guid.Parse(request.Id)) 
+            ?? throw new RpcException(new Status(StatusCode.NotFound, $"Auction with ID {request.Id} not found"));
+
+        var response = new GrpcAuctionResponse
+        {
+            Auction = new GrpcAuctionModel
+            {
+                Id = auction.Id.ToString(),
+                Seller = auction.Seller,
+                AuctionEnd = auction.AuctionEnd.ToString(),
+                ReservePrice = auction.ReservePrice,
+            }
+        };
+
+        return response;
+    }
+
+}
